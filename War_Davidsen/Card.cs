92cca36965bfe9813ff88cb332c6384d9266f4d3
Caleb Davidsen <127970819+CalebDavisen@users.noreply.github.com>
2023-11-29 13:12:19 -0600

    Add files via upload

---
+++ War_Davidsen/Card.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace War_Davidsen
+{
+
+    enum Suit
+    {
+        Diamond,
+        Hearts,
+        Spades,
+        Clubs
+    }
+
+    enum Face
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    /// <summary>
+    /// Represents a card in a deck.s
+    /// </summary>
+    class Card
+    {
+        /// <summary>
+        /// The suit of the card.
+        /// </summary>
+        public Suit Suit { get; set;  }
+
+        /// <summary>
+        /// The face of the card.
+        /// </summary>
+        public Face Face { get; set;  }
+
+        /// <summary>
+        /// Cunstructs a new card object with a given suit and face.
+        /// </summary>
+        /// <param name="suit">Suit of the card.</param>
+        /// <param name="face">Face of the card.</param>
+        public Card(Suit suit, Face face)
+        {
+            this.Suit = suit;
+            this.Face = face;
+        }
+
+        /// <summary>
+        /// Compares the current card to the passed card.
+        /// </summary>
+        /// <param name="other">The other card to compair to.</param>
+        /// <returns>Return a negative number if current card is smaller 
+        /// returns positive number if current card is bigger.</returns>
+        public int CompareTo(Card other)
+        {
+            return this.Face - other.Face;
+        }
+    }
+}
---
+++ War_Davidsen/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace War_Davidsen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playButton = new System.Windows.Forms.Button();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.computerScoreTextBox = new System.Windows.Forms.TextBox();
+            this.playerScoreTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.playerPictureBox = new System.Windows.Forms.PictureBox();
+            this.computerPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.playerPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(65, 328);
+            this.playButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(112, 35);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play Card";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // restartButton
+            // 
+            this.restartButton.Enabled = false;
+            this.restartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restartButton.Location = new System.Drawing.Point(231, 328);
+            this.restartButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(112, 35);
+            this.restartButton.TabIndex = 1;
+            this.restartButton.Text = "Restart";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(148, 373);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(112, 35);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // computerScoreTextBox
+            // 
+            this.computerScoreTextBox.Enabled = false;
+            this.computerScoreTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.computerScoreTextBox.Location = new System.Drawing.Point(228, 61);
+            this.computerScoreTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.computerScoreTextBox.Name = "computerScoreTextBox";
+            this.computerScoreTextBox.Size = new System.Drawing.Size(99, 26);
+            this.computerScoreTextBox.TabIndex = 3;
+            this.computerScoreTextBox.Text = "0";
+            this.computerScoreTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // playerScoreTextBox
+            // 
+            this.playerScoreTextBox.Enabled = false;
+            this.playerScoreTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerScoreTextBox.Location = new System.Drawing.Point(228, 274);
+            this.playerScoreTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.playerScoreTextBox.Name = "playerScoreTextBox";
+            this.playerScoreTextBox.Size = new System.Drawing.Size(98, 26);
+            this.playerScoreTextBox.TabIndex = 4;
+            this.playerScoreTextBox.Text = "0";
+            this.playerScoreTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 64);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Computer\'s Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(95, 277);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Player\'s Score:";
+            // 
+            // playerPictureBox
+            // 
+            this.playerPictureBox.Image = global::War_Davidsen.Properties.Resources.beach_back;
+            this.playerPictureBox.Location = new System.Drawing.Point(232, 168);
+            this.playerPictureBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.playerPictureBox.Name = "playerPictureBox";
+            this.playerPictureBox.Size = new System.Drawing.Size(71, 96);
+            this.playerPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.playerPictureBox.TabIndex = 7;
+            this.playerPictureBox.TabStop = false;
+            // 
+            // computerPictureBox
+            // 
+            this.computerPictureBox.Image = global::War_Davidsen.Properties.Resources.beach_back;
+            this.computerPictureBox.Location = new System.Drawing.Point(107, 103);
+            this.computerPictureBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.computerPictureBox.Name = "computerPictureBox";
+            this.computerPictureBox.Size = new System.Drawing.Size(71, 96);
+            this.computerPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.computerPictureBox.TabIndex = 8;
+            this.computerPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 422);
+            this.Controls.Add(this.computerPictureBox);
+            this.Controls.Add(this.playerPictureBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.playerScoreTextBox);
+            this.Controls.Add(this.computerScoreTextBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.playButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "War";
+            this.Load += new System.EventHandler(this.restartButton_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.playerPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox computerScoreTextBox;
+        private System.Windows.Forms.TextBox playerScoreTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox playerPictureBox;
+        private System.Windows.Forms.PictureBox computerPictureBox;
+    }
+}
+
